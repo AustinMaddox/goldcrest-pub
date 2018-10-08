549e3d0e1c3e915c1bd1328a0b60a667a0b2e609
AustinMaddox <austin@maddoxbox.com>
2018-10-08 09:44:34 -0600

    Move demos into directories

--- public/demo1/publish.php
+++ public/demo1/publish.php
@@ -1,6 +1,6 @@
 <?php
 
-include_once dirname(__FILE__) . '/../vendor/autoload.php';
+include_once dirname(__FILE__) . '/../../vendor/autoload.php';
 
 $redis_client = new Predis\Client([
     'host'   => 'redis-4-0',
